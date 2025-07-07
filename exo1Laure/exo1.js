e8530c3ddf618a0ffa57146d8ca4dfcfb0836f19
VincentComier <V.Comier@hotmail.fr>
2025-07-07 10:51:53 +0200

    exo1

---
+++ exo1Laure/exo1.js
@@ -0,0 +1,9 @@
+let nombre = 6;
+
+if (nombre > 0) {
+  console.log("Le nombre est positif.");
+} else if (nombre < 0) {
+  console.log("Le nombre est négatif.");
+} else {
+  console.log("Le nombre est zéro.");
+}
