8a6e24b627b4dbe7bfc3bdffd6fdbbff771dce51
VincentComier <V.Comier@hotmail.fr>
2025-07-07 11:24:54 +0200

    exo 3

---
+++ exo3Laure/script.js
@@ -0,0 +1,31 @@
+/*
+int tmp = 15; 
+
+if (tmp < 0)   
+    printf("Freezing weather.");
+else if (tmp < 10)  
+    printf("Very cold weather.");
+else if (tmp < 20)  
+    printf("Cold weather.");   .
+else if (tmp < 30)  
+    printf("Normal in temp."); 
+else if (tmp < 40)   
+    printf("Its Hot."); 
+else
+   printf("Its very hot."); */
+
+let tmp = 15
+
+if (tmp < 0) {
+    console.log("Freezing weather.")
+} else if (tmp < 10) {
+    console.log("Very cold weather.")
+} else if (tmp < 20) {
+    console.log("Cold weather.")
+} else if (tmp < 30) {
+    console.log("Normal in temps.")
+} else if (tmp < 40) {
+    console.log("It's Hot.")
+} else {
+    console.log("It's very hot.")
+}
