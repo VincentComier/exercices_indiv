d75d1befefff339d2f02b4b2d6dc1244d6c91171
VincentComier <V.Comier@hotmail.fr>
2025-07-09 10:35:53 +0200

    premier exo

---
+++ exo1Vi/exo1.js
@@ -0,0 +1,18 @@
+//Écris une boucle FOR qui affiche dans la console tous les nombres de 0 à 99.
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+}
+//Écris le même compteur avec une boucle WHILE.
+let i = 0;
+while (i < 100) {
+    console.log(i);
+    i++;
+}
+//Ecris un compteur qui affiche tous les nombres de 50 à 99 (avec la boucle de ton choix).
+for (let i = 50; i < 100; i++) {
+    console.log(i);
+}
+//Ecris un compteur qui affiche tous les nombre de 49 à 1 (avec la boucle de ton choix).
+for (let i = 49; i > 0; i--) {
+    console.log(i);
+}
