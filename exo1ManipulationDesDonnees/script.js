9da170875db0c6ed9c6d93bf54e68cba5ac239ea
VincentComier <V.Comier@hotmail.fr>
2025-07-15 10:02:45 +0200

    Exo1 Manipulation de données

---
+++ exo1ManipulationDesDonnees/script.js
@@ -0,0 +1,16 @@
+// Avec l’annexe ci-dessous :
+
+     const candidates = ['Le Filip','Ruby On The Nail','Leona Winter','Lula Strega','Misty Phoenix',
+       'Perseo','Norma Bell','Edeha Noire','Magnetica','Afrodite Amour'];
+
+//     Accède au tableau pour afficher dans la console “Le Filip”
+//     Accède au tableau pour afficher dans la console “Edeha Noire”
+//     Parcours le tableau pour afficher dans la console ligne par ligne chaque nom des candidates
+//     (Bonus) Parcours le tableau candidates pour afficher la liste des candidates dans une page HTML :
+
+console.log("Accède au tableau pour afficher dans la console “Le Filip” :" ,candidates[0])
+console.log("Accède au tableau pour afficher dans la console “Edeha Noire” :" ,candidates[7])
+
+for (item of candidates) {
+    console.log(item)
+}
